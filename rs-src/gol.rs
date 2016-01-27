80f94129a7466b17c0e6f22cb3375741b2380b84
Tim C. Schroeder <tim@blitzcode.net>
2016-01-27 20:09:26 +0100

    Split GoL computation into interior and boundary regions, using 1D and 2D indexing, respectively

--- rs-src/gol.rs
+++ rs-src/gol.rs
@@ -29,7 +29,6 @@ pub extern fn gol_randomize() -> () {
 
 #[no_mangle]
 pub extern fn gol_step() -> () {
-
     // Allocate new grid without wasting time setting it to any value
     let grid_size = (GRID_WDH * GRID_WDH) as usize;
     let mut new_grid = Vec::with_capacity(grid_size);
@@ -37,8 +36,22 @@ pub extern fn gol_step() -> () {
 
     let mut grid = GRID.lock().unwrap();
 
-    for y in 0..GRID_WDH {
-        for x in 0..GRID_WDH {
+    // Compute the four borders
+    for side in 0..4 {
+        let mut x;
+        let mut y;
+        let xincr;
+        let yincr;
+        match side {
+            0 => { x = 0           ; y = 0           ; xincr = 1; yincr = 0; },
+            1 => { x = 0           ; y = GRID_WDH - 1; xincr = 1; yincr = 0; },
+            2 => { x = 0           ; y = 0           ; xincr = 0; yincr = 1; },
+            3 => { x = GRID_WDH - 1; y = 0           ; xincr = 0; yincr = 1; },
+            _ => panic!("invalid side")
+        }
+
+        while x < GRID_WDH && y < GRID_WDH {
+            // We're at the boundary, wrap
             let torus_idx = |x: i32, y: i32| -> u8 {
                 let wrapx = if x < 0 { GRID_WDH - 1 } else { if x > GRID_WDH - 1 { 0 } else { x } };
                 let wrapy = if y < 0 { GRID_WDH - 1 } else { if y > GRID_WDH - 1 { 0 } else { y } };
@@ -46,6 +59,7 @@ pub extern fn gol_step() -> () {
                 grid[idx as usize]
             };
 
+            // 2D indexing, faster to wrap
             let idx = (x + y * GRID_WDH) as usize;
             let alive = grid[idx];
             let alive_nb = torus_idx(x + 1, y    ) +
@@ -58,6 +72,29 @@ pub extern fn gol_step() -> () {
                            torus_idx(x - 1, y + 1);
 
             new_grid[idx] = if alive_nb == 3 || (alive == 1 && alive_nb == 2) { 1 } else { 0 };
+
+            x += xincr;
+            y += yincr;
+        }
+    }
+
+    // Compute interior
+    for y in 1..GRID_WDH - 1 {
+        for x in 1..GRID_WDH - 1 {
+            // 1D indexing, no wrapping needed
+            let idx = x + y * GRID_WDH;
+            let alive = grid[idx as usize];
+            let alive_nb = grid[(idx + 1           ) as usize] +
+                           grid[(idx - 1           ) as usize] +
+                           grid[(idx     + GRID_WDH) as usize] +
+                           grid[(idx     - GRID_WDH) as usize] +
+                           grid[(idx + 1 + GRID_WDH) as usize] +
+                           grid[(idx + 1 - GRID_WDH) as usize] +
+                           grid[(idx - 1 + GRID_WDH) as usize] +
+                           grid[(idx - 1 - GRID_WDH) as usize];
+
+            new_grid[idx as usize] =
+                if alive_nb == 3 || (alive == 1 && alive_nb == 2) { 1 } else { 0 };
         }
     }
 
@@ -66,7 +103,6 @@ pub extern fn gol_step() -> () {
 
 #[no_mangle]
 pub extern fn gol_draw(w: i32, h: i32, fb: *mut u32) -> () {
-
     // Clear background
     unsafe { ptr::write_bytes(fb, 64, (w * h) as usize); }
 
@@ -95,7 +131,6 @@ pub extern fn gol_draw(w: i32, h: i32, fb: *mut u32) -> () {
 
 #[no_mangle]
 pub extern fn gol_set_pattern(w: i32, h: i32, pat: *mut u8) -> () {
-
     let mut grid = GRID.lock().unwrap();
 
     // Clear grid

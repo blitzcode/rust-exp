fa5560fa8d4809bcdd02fcb026168b5f019ce702
Tim C. Schroeder <tim@blitzcode.net>
2016-01-29 20:48:03 +0100

    Fixed N-body error, improved setting up initial particles, debug code

--- rs-src/nbody.rs
+++ rs-src/nbody.rs
@@ -1,6 +1,9 @@
 
 use std::sync::Mutex;
 use std::ptr;
+use rand;
+use rand::distributions::{IndependentSample, Range};
+use std::f32::consts;
 
 #[derive(Clone, Copy)]
 struct Particle {
@@ -11,15 +14,43 @@ struct Particle {
     m  : f32
 }
 
+fn uniform_sample_disk(x: &mut f32, y: &mut f32) {
+    let r = x.sqrt();
+    let theta = 2.0 * consts::PI * (* y);
+    * x = r * theta.cos();
+    * y = r * theta.sin();
+}
+
 lazy_static! {
     static ref PARTICLES: Mutex<Vec<Particle>> = {
         let mut v = Vec::new();
 
+        /*
         v.push(Particle { px: 0.5, py: 0.5, vx: 0.0, vy: 0.0, m: 1000.0 });
 
         v.push(Particle { px: -8.23656 , py: 1.78778 , vx: -4.61142 , vy: -31.2847, m: 10.0 });
         v.push(Particle { px: -4.12657, py:  -2.99729, vx:  19.069, vy:  -25.2264, m: 10.0 });
         v.push(Particle { px: -6.00204, py: 27.3733, vx: -30.7359, vy: -7.4366, m: 10.0 });
+        */
+
+        let mut rng = rand::thread_rng();
+        let vel = Range::new(-3.5, 3.5);
+        let mass = Range::new(0.1, 1.5);
+        let u = Range::new(0.0, 1.0);
+
+        for _ in 0..1000 {
+            let mut x : f32 = u.ind_sample(&mut rng);
+            let mut y : f32 = u.ind_sample(&mut rng);
+            uniform_sample_disk(&mut x, &mut y);
+            x *= 20.0;
+            y *= 20.0;
+
+            v.push(Particle { px: x,
+                              py: y,
+                              vx: vel.ind_sample(&mut rng),
+                              vy: vel.ind_sample(&mut rng),
+                              m:  mass.ind_sample(&mut rng) });
+        }
 
         Mutex::new(v)
     };
@@ -53,14 +84,12 @@ pub extern fn nb_step() -> () {
                 let dx = b.px - a.px;
                 let dy = b.py - a.py;
                 let dist = (dx * dx + dy * dy).sqrt();
-                let dxn = dx / dist;
-                let dyn = dy / dist;
 
                 let eps = 0.00001; // Softening factor, prevent singularities
-                let f = (a.m * b.m) / (dist * dist + eps);
+                let f = a.m * b.m / (dist * dist + eps);
 
-                mforces[i].fx += f * dxn;
-                mforces[i].fy += f * dyn;
+                mforces[i].fx += f * dx; // / dist;
+                mforces[i].fy += f * dy; // / dist;
             }
         }
 
@@ -122,6 +151,15 @@ pub extern fn nb_draw(w: i32, h: i32, fb: *mut u32) -> () {
         unsafe {
             * fb.offset(idx as isize) = 0x00FFFFFF;
         }
+    }
+
+    // Debug draw center cross
+    unsafe {
+        * fb.offset((w / 2 + 0 + (h / 2 + 0) * w) as isize) = 0x00FF00FF;
+        * fb.offset((w / 2 + 1 + (h / 2 + 0) * w) as isize) = 0x00FF00FF;
+        * fb.offset((w / 2 + 0 + (h / 2 + 1) * w) as isize) = 0x00FF00FF;
+        * fb.offset((w / 2 - 1 + (h / 2 + 0) * w) as isize) = 0x00FF00FF;
+        * fb.offset((w / 2 + 0 + (h / 2 - 1) * w) as isize) = 0x00FF00FF;
     }
 }
 

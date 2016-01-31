501bbc870800904c223108edd5832f95063acb83
Tim C. Schroeder <tim@blitzcode.net>
2016-01-31 16:10:33 +0100

    Made Theta run time tweakable

--- hs-src/RustNBodyExperiment.hs
+++ hs-src/RustNBodyExperiment.hs
@@ -33,6 +33,7 @@ data RustNBodyExperiment = RustNBodyExperiment
     { _rnbNumSteps :: !Int
     , _rnbTimes    :: !(BS.BoundedSequence Double)
     , _rnbTimeStep :: !Double
+    , _rnbTheta    :: !Double
     }
 
 makeLenses ''RustNBodyExperiment
@@ -42,13 +43,14 @@ instance Experiment RustNBodyExperiment where
                           f $ RustNBodyExperiment { _rnbNumSteps = 0
                                                   , _rnbTimes    = BS.empty 30
                                                   , _rnbTimeStep = 0.01
+                                                  , _rnbTheta    = 0.85
                                                   }
     experimentName _ = "RustNBody"
     experimentDraw fb _tick = do
-        dt <- use rnbTimeStep
+        dt    <- use rnbTimeStep
+        theta <- use rnbTheta
         -- Simulate first
-        let theta = 0.85 :: Float
-        time <- fst <$> liftIO (timeIt . nbStepBarnesHut (realToFrac theta) . realToFrac $ dt)
+        time <- fst <$> liftIO (timeIt $ nbStepBarnesHut (realToFrac theta) (realToFrac dt))
         void . liftIO . fillFrameBuffer fb $ \w h vec ->
             VSM.unsafeWith vec $ \pvec ->
                 nbDraw (fromIntegral w) (fromIntegral h) pvec
@@ -59,8 +61,8 @@ instance Experiment RustNBodyExperiment where
         let avgtime = fromMaybe 1 . median . BS.toList $ _rnbTimes
         np <- liftIO (fromIntegral <$> nbNumParticles :: IO Int)
         return $ printf ( "%i Steps, %.2fms, %.1f Steps/Second\n" ++
-                         "%s Particles | [QWE] Setup Particles | " ++
-                         "Time Step [T][t]: %.4f"
+                         "%s P | [QWE] Scene | Time Step [T][t]: %.4f | " ++
+                         "Theta [A][a]: %.2f"
                         )
                         _rnbNumSteps
                         (avgtime * 1000)
@@ -70,6 +72,7 @@ instance Experiment RustNBodyExperiment where
                           else show np
                         )
                         _rnbTimeStep
+                        _rnbTheta
     experimentGLFWEvent ev = do
         case ev of
             GLFWEventKey _win k _sc ks mk | ks == GLFW.KeyState'Pressed ->
@@ -79,6 +82,10 @@ instance Experiment RustNBodyExperiment where
                     GLFW.Key'E -> liftIO $ nbStableOrbits 5 5.0 40.0
                     GLFW.Key'T | GLFW.modifierKeysShift mk -> rnbTimeStep //= 2
                                | otherwise                 -> rnbTimeStep  *= 2
+                    GLFW.Key'A | GLFW.modifierKeysShift mk ->
+                                     rnbTheta %= max 0.0 . min 0.95 . (\x -> x - 0.05)
+                               | otherwise                 ->
+                                     rnbTheta %= max 0.0 . min 0.95 . (\x -> x + 0.05)
                     _          -> return ()
             _ -> return ()
 
